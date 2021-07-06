7d7fbec97f5312b4ca42019358c7a240db6c1748
radrow <radrowicki@gmail.com>
2021-07-06 17:59:07 +0200

    Add autoupdate script

---
+++ update.sh
@@ -0,0 +1,12 @@
+sophia_vsn=$1
+
+[ -z $sophia_vsn ] && echo -e "USAGE: ./update.sh SOPHIA_VSN\nEXAMPLE: ./update.sh 6.0.2" && exit 1
+
+sed -i "s#aesophia.git\", {tag, \".*\"}}}#aesophia.git\", {tag, \"v$sophia_vsn\"}}}#g" rebar.config
+sed -i "s#{vsn, \".*\"}#{vsn, \"$sophia_vsn\"}#g" src/aesophia_cli.app.src
+./rebar3 upgrade
+./rebar3 escriptize
+mkdir priv/bin/v$sophia_vsn
+cp aesophia_cli priv/bin/v$sophia_vsn/
+test/test_cli.sh && echo -e "\e[1mDone! Remember to update CHANGELOG.md\e[0m"
+
