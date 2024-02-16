e0a1730c234c4c2cf57889b71bf9496374a0b840
Hans Svensson <hanssv@gmail.com>
2024-02-16 15:28:18 +0100

    Update checked in escripts to OTP-24 (#87)
    * Fix install_all script
    
    * Compile priv/bin escripts with OTP-24

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-rm -rf _build
-mkdir -p all_versions
+if [ ! -d all_versions ]; then
+  echo "No folder 'all_versions' found. You need to compile first!"
+  exit 1
+fi
+
 for VSN in `git tag --list`; do
   if [ -d priv/bin/${VSN} ]; then
     echo "Saving as priv/bin/${VSN}/aesophia_cli"
